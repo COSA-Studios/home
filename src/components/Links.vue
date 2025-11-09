cabc9480329e0f88914b7dadaa22f4495e67a66d
Hanze-C <hanze2010@outlook.com>
2025-11-09 17:54:52 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -35,7 +35,7 @@
 <script setup lang="ts">
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, BellRegular, Eye, Video, Train, CameraRetro, Compass, Music, Book, StickyNote, PencilRuler, PenSquare, PaintBrush, Images, Envelope, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, BellRegular, Eye, Search, Video, Train, CameraRetro, Compass, Music, Book, StickyNote, PencilRuler, PenSquare, PaintBrush, Images, Envelope, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -73,6 +73,7 @@ const siteIcon = {
   StickyNote,
   PaintBrush,
   PenSquare,
+  Search,
   CameraRetro,
   PencilRuler,
   Book,
